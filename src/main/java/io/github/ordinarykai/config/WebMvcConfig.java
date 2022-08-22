a2f5d79e13a8d610c247dc314ebe6443539a3e98
wukai <2115114903@qq.com>
2022-08-22 17:31:27 +0800

    update

--- src/main/java/io/github/ordinarykai/config/WebMvcConfig.java
+++ src/main/java/io/github/ordinarykai/config/WebMvcConfig.java
@@ -18,17 +18,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Resource
     FileUploadProperties properties;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 解决跨域问题
-        // springboot版本是2.5.8，设置allowedOrigins("*")且.allowCredentials(true)会报错
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
-
     /**
      * 配置静态资源的访问
      */
--- src/main/java/io/github/ordinarykai/constant/CommonConstant.java
+++ src/main/java/io/github/ordinarykai/constant/CommonConstant.java
@@ -8,32 +8,10 @@ package io.github.ordinarykai.constant;
  */
 public interface CommonConstant {
 
-    String TOKEN = "token";
-
     /**
      * 默认密码
      */
     String DEFAULT_PWD = "123456";
-
-    /**
-     * 用户登录过期时间，单位s
-     */
-    long EXPIRE_TIME = 24 * 60 * 60;
-
-    /**
-     * java.time.LocalTime格式化文本
-     */
-    String LOCAL_TIME_PATTERN = "HH:mm:ss";
-
-    /**
-     * java.time.LocalDate格式化文本
-     */
-    String LOCAL_DATE_PATTERN = "yyyy-MM-dd";
-
-    /**
-     * java.time.LocalDateTime格式化文本
-     */
-    String LOCAL_DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 禁用
@@ -44,15 +22,5 @@ public interface CommonConstant {
      * 启用标志
      */
     Integer ENABLE = 1;
-
-    /**
-     * 异常信息：系统异常，该数据不存在
-     */
-    String DATA_NOT_FOUND = "系统异常，该数据不存在";
-
-    /**
-     * 正式环境标识符
-     */
-    String PRO_ENV = "run";
 
 }
